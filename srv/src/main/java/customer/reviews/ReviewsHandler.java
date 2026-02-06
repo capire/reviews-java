eb99102b37472bcec9e6beebd3cb6200d9fad39f
Johannes Vogt <akatuoro6@gmail.com>
2026-02-06 13:50:40 +0100

    review (#6)

--- srv/src/main/java/customer/reviews/ReviewsHandler.java
+++ srv/src/main/java/customer/reviews/ReviewsHandler.java
@@ -1,24 +1,17 @@
 package customer.reviews;
 
-import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import com.sap.cds.Result;
-import com.sap.cds.Row;
-import com.sap.cds.ql.CQL;
 import com.sap.cds.ql.Delete;
 import com.sap.cds.ql.Insert;
 import com.sap.cds.ql.Select;
 import com.sap.cds.ql.Update;
 import com.sap.cds.ql.cqn.CqnDelete;
 import com.sap.cds.ql.cqn.CqnUpdate;
-import com.sap.cds.services.EventContext;
 import com.sap.cds.services.ServiceException;
 import com.sap.cds.services.cds.CqnService;
 import com.sap.cds.services.handler.EventHandler;
@@ -27,13 +20,16 @@ import com.sap.cds.services.handler.annotations.On;
 import com.sap.cds.services.handler.annotations.ServiceName;
 import com.sap.cds.services.persistence.PersistenceService;
 
-import cds.gen.sap.capire.reviews.Likes;
-import cds.gen.sap.capire.reviews.Likes_;
+import cds.gen.reviewsservice.Likes;
+import cds.gen.reviewsservice.Likes_;
+import cds.gen.reviewsservice.Review;
 import cds.gen.reviewsservice.AverageRatingsChanged;
 import cds.gen.reviewsservice.AverageRatingsChangedContext;
+import cds.gen.reviewsservice.AverageRatings_;
 import cds.gen.reviewsservice.ReviewsService_;
 import cds.gen.reviewsservice.LikeContext;
 import cds.gen.reviewsservice.Reviews;
+import cds.gen.reviewsservice.ReviewsService;
 import cds.gen.reviewsservice.Reviews_;
 import cds.gen.reviewsservice.UnlikeContext;
 
@@ -42,101 +38,83 @@ import cds.gen.reviewsservice.UnlikeContext;
 public class ReviewsHandler implements EventHandler {
 
   @Autowired
-  @Qualifier(ReviewsService_.CDS_NAME)
-  CqnService service;
+  ReviewsService service;
 
-  private final PersistenceService persistenceService;
+  @Autowired
+  PersistenceService persistenceService;
+
   private final Random random;
 
-  public ReviewsHandler(PersistenceService persistenceService) {
-    this.persistenceService = persistenceService;
+  public ReviewsHandler() {
     this.random = new Random(System.currentTimeMillis());
   }
 
   @On(event = { CqnService.EVENT_CREATE, CqnService.EVENT_UPDATE } )
-  public void onUpdateReview(EventContext context, Reviews review) {
-    if(review.getRating()==null)
-      review.setRating(random.nextInt(4)+1);
+  public void onUpdateReview(Reviews review) {
+    if(review.getRating() == null)
+      review.setRating(random.nextInt(4) + 1);
   }
 
-  @On(service = ReviewsService_.CDS_NAME)
+  @On
   public void onLike(LikeContext context) {
-    String reviewer = context.getReview().getReviewer();
-    String subject = context.getReview().getSubject();
+    Review review = context.getReview();
+    String reviewer = review.getReviewer();
+    String subject = review.getSubject();
+    Likes like = Likes.create();
+    like.setUser(context.getUserInfo().getName());
+    like.setReviewReviewer(reviewer);
+    like.setReviewSubject(subject);
+
     try {
-      persistenceService.run(Insert.into(Likes_.CDS_NAME)
-        .entry(Map.of(
-          "user", context.getUserInfo().getName(), 
-          "review_reviewer", reviewer,
-          "review", Map.of("subject",subject))));
+      persistenceService.run(Insert.into(Likes_.class).entry(like));
 
-      Map<String, Object> filter = new HashMap<>();
-      filter.put(Reviews.REVIEWER, reviewer);
-      filter.put(Reviews.SUBJECT, subject);
-      
-      CqnUpdate update = Update.entity(cds.gen.sap.capire.reviews.Reviews_.CDS_NAME)
-        .set(Reviews.LIKED, CQL.get(Reviews.LIKED).plus(1))
-        .matching(filter);
-
+      CqnUpdate update = Update.entity(Reviews_.class)
+        .set(r -> r.liked(), liked -> liked.plus(1))
+        .matching(review);
       Result updateResult = persistenceService.run(update);
-      if(updateResult.rowCount()!=1)
+      if(updateResult.rowCount() != 1)
         throw new ServiceException("Failed to update the liked counter");
 
     } catch(Exception e) {
-      if(e.getMessage().startsWith("Unique constraint violated"))
+      if (e.getMessage().startsWith("Unique constraint violated"))
         throw new ServiceException("You already liked that review", e);
       throw new ServiceException("Failed to perform the operation", e);
     }
     context.setCompleted();
   }
 
-  @On(service = ReviewsService_.CDS_NAME)
+  @On
   public void onUnlike(UnlikeContext context) {
-    String reviewer = context.getReview().getReviewer();
-    String subject = context.getReview().getSubject();
-    try {
-      CqnDelete delete = Delete.from(Likes_.CDS_NAME)
-        .where(b -> b.get(Likes.USER).eq(CQL.param("user"))
-          .and(b.get(Likes.REVIEW_REVIEWER).eq(CQL.param("reviewer")))
-          .and(b.get(Likes.REVIEW_SUBJECT).eq(CQL.param("subject"))));
+    Review review = context.getReview();
+    String reviewer = review.getReviewer();
+    String subject = review.getSubject();
+    Likes like = Likes.create();
+    like.setUser(context.getUserInfo().getName());
+    like.setReviewReviewer(reviewer);
+    like.setReviewSubject(subject);
 
-      Map<String, Object> paramValues = new HashMap<>();
-      paramValues.put("user", context.getUserInfo().getName());
-      paramValues.put("reviewer", reviewer);
-      paramValues.put("subject", subject);
+    CqnDelete delete = Delete.from(Likes_.class)
+      .matching(like);
 
-      Result deleteResult = persistenceService.run(delete, paramValues);
-      if(deleteResult.rowCount()!=1)
-        throw new ServiceException("Failed to delete the like");
+    Result deleteResult = persistenceService.run(delete);
+    if(deleteResult.rowCount() != 1)
+      throw new ServiceException("You have not liked that review");
 
-      Map<String, Object> filter = new HashMap<>();
-      filter.put(Reviews.REVIEWER, reviewer);
-      filter.put(Reviews.SUBJECT, subject);
+    CqnUpdate update = Update.entity(Reviews_.class)
+      .set(r -> r.liked(), liked -> liked.minus(1))
+      .matching(review);
+    persistenceService.run(update);
 
-      CqnUpdate update = Update.entity(cds.gen.sap.capire.reviews.Reviews_.CDS_NAME)
-        .set(Reviews.LIKED, CQL.get(Reviews.LIKED).minus(1))
-        .matching(filter);
-
-      Result updateResult = persistenceService.run(update);
-      if(updateResult.rowCount()!=1)
-        throw new ServiceException("Failed to update the liked counter");
-
-    } catch(Exception e) {
-      throw new ServiceException("Failed to perform the operation", e);
-    }
     context.setCompleted();
   }
 
   @After(event = { CqnService.EVENT_CREATE, CqnService.EVENT_UPDATE, CqnService.EVENT_DELETE } )
-  public void afterChangeReview(EventContext context, Reviews review) {
-    Result result = service.run(Select.from(Reviews_.CDS_NAME)
-      .columns(r -> CQL.count(r.get("subject")).as("count"), r -> CQL.average(r.get("rating")).as("avg"))
-      .where(o -> o.get("subject").eq(review.getSubject())));
-    Row selected = result.single();
+  public void afterChangeReview(Reviews review) {
     String subject = review.getSubject();
-    Long count = (Long) selected.get("count");
-    BigDecimal avgRating = (BigDecimal) selected.get("avg");
-    sendReviewedMessage(subject, count.intValue(), avgRating.intValue());
+    var result = service.run(Select.from(AverageRatings_.class)
+      .where(r -> r.subject().eq(subject)));
+    var selected = result.single();
+    sendReviewedMessage(subject, selected.getReviews(), selected.getRating());
   }
 
   private void sendReviewedMessage(String subject, Integer count, Integer avgRating) {
